097b49fa7a45cb6c0f80c26bd604ef94c331eac2
Zhao Tianyu <davidxk@qq.com>
2018-02-23 21:28:37 -0800

    Bug fix and optimization for bidirectional, use hash set only when necessary

--- shortest_path/py/bidirectional.py
+++ shortest_path/py/bidirectional.py
@@ -1,24 +1,24 @@
 # Tested
 # Verified
 def bidirectional(adjList, source, target):
-    fronts = [set([source]), set([target])]
+    fronts = [[source], [target]]
     visited = [set([source]), set([target])]
     cnt = [0, 0]
     prev = [{source: None}, {target: None}]
-    border = set()
+    border = []
     if source == target:
-        border.add(source)
+        border.append(source)
     while all(fronts) and not border: 
         smaller = 0 if len(fronts[0]) < len(fronts[1]) else 1
-        children = set()
+        children = []
         cnt[smaller] += 1
         for node in fronts[smaller]:
             for child in adjList[node]:
-                if child in fronts[not smaller]:
-                    border.add(child)
+                if child in visited[not smaller]:
+                    border.append(child)
                 if child not in visited[smaller]:
-                    visited[smaller].add(node)
-                    children.add(child)
+                    visited[smaller].add(child)
+                    children.append(child)
                     prev[smaller][child] = node
         fronts[smaller] = children
     for node, parent in prev[1].items():
