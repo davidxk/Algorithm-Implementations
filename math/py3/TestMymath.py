36f7fb704f4e22a033ef1da6c44bf01ff105a205
davidxk <davidxk23@gmail.com>
2020-01-26 18:05:54 -0800

    Added Extended Euclidean Algorithm, Modular multiplicative inverse, Chinese Remainder Theorem, and fraction ceiling implementations

--- math/py3/TestMymath.py
+++ math/py3/TestMymath.py
@@ -1,5 +1,5 @@
 import unittest
-import fractions
+import math
 import random
 import mymath
 
@@ -7,7 +7,33 @@ class TestMymath(unittest.TestCase):
     def testGCD(self):
         for i in range(100):
             a, b = random.randrange(100000), random.randrange(100000)
-            self.assertEqual(mymath.gcd(a, b), fractions.gcd(a, b))
+            self.assertEqual(mymath.gcd(a, b), math.gcd(a, b))
+
+    def testExtendedEuclidean(self):
+        for i in range(100):
+            a, b = random.randrange(100000), random.randrange(100000)
+            r, s, t = mymath.egcd(a, b)
+            self.assertEqual(r, math.gcd(a, b))
+            self.assertEqual(a * s + b * t, r)
+
+    def testModInv(self):
+        primes = [2, 3, 5, 7, 11, 13, 17, 19]
+        for prime in primes:
+            for num in range(1, prime):
+                self.assertEqual(num * mymath.mod_inv(num, prime) % prime, 1)
+
+    def testChineseRemainderTheorem(self):
+        moduli, remainders = [3, 5, 7], [2, 3, 2]
+        self.assertEqual(mymath.chinese_remainder_theorem(moduli, remainders), 23)
+
+    def testFracCeiling(self):
+        for i in range(100):
+            a, b = random.randrange(100000), random.randrange(100000)
+            big, small = max(a, b), min(a, b)
+            expected = big // small
+            if big % small != 0:
+                expected += 1
+            self.assertEqual(mymath.frac_ceiling(big, small), expected)
 
 if __name__ == "__main__":
     unittest.main()
--- math/py3/mymath.py
+++ math/py3/mymath.py
@@ -1,5 +1,39 @@
+import functools
 
 def gcd(large, small):
     while small:
         large, small = small, large % small
     return large
+
+# Extended Euclidean Algorithm
+# r = gcd(a, b) is the Bezout identity
+# s, t are the Bezout coefficents where a*s + b*t = r
+def egcd(a, b):
+    r, nr = a, b
+    s, ns = 1, 0
+    t, nt = 0, 1
+    while nr != 0:
+        quotient = r // nr
+        r, nr = nr, r - quotient * nr
+        s, ns = ns, s - quotient * ns
+        t, nt = nt, t - quotient * nt
+    return r, s, t
+
+# Modular multiplicative inverse
+def mod_inv(a, n):
+    _, s, _ = egcd(a, n)
+    return s % n
+
+# The moduli must be pairwise coprime
+def chinese_remainder_theorem(moduli, remainders):
+    prod = functools.reduce(lambda a, b: a * b, moduli)
+    x = 0
+    for i, mi in enumerate(moduli):
+        Mi = prod // mi
+        MiInv = mod_inv(Mi, mi)
+        x += remainders[i] * Mi * MiInv
+    x %= prod
+    return x
+
+def frac_ceiling(quotient, denominator):
+    return 1 + (quotient - 1) // denominator
